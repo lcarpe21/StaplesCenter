c5c73a6e44b567d82607def8dcbff175c7c0f064
”Levar <lfreder6@uncc.edu>
2020-04-10 17:18:29 +0000

    Add blogger adds a comment scenario 1

--- spec/integration/blogger_adds_an_article_spec.rb
+++ spec/integration/blogger_adds_an_article_spec.rb
@@ -20,3 +20,14 @@ feature "Blogger adds an article" do
         expect(page).to have_content("This is a new Capybara article")
     end
 end
+
+feature "Blogger adds a comment after creating article" do
+    scenario "Blogger successfully navigates to the new articles page from the listing articles page" do
+        visit articles_path
+        expect(page).to have_content("Listing articles")
+        click_link "New article"
+        expect(page).to have_content("New Article")
+        expect(page).to have_field("Title")
+        expect(page).to have_field("Text")
+    end
+end
