bd19d7881b0e84adf101feb4686e1df80108df7f
jdannemi <jdannemi@uncc.edu>
2020-04-10 15:23:11 +0000

    Added Edit functions to controller,comment/edit,show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,21 @@ class CommentsController < ApplicationController
         
     end
     
+    def edit
+        @article = Article.find(params[:article_id])
+        @comment = Comment.find(params[:id])
+    end
+    
+    def update
+        @article = Article.find(params[:article_id])
+        @comment = Comment.find(params[:id])
+        if @comment.update(comment_params)
+        redirect_to @comment
+        else
+        render 'edit'
+        end
+    end
+    
     private
         def comment_params
             params.require(:comment).permit(:commenter, :body)
