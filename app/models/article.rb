23a5c73dc6e799730a9c2f18799947476b9c9278
jdannemi <jdannemi@uncc.edu>
2020-04-28 21:16:41 +0000

    Added photo upload and database functionality

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
     belongs_to :user
     
+    has_one :photo
+    
     has_many :comments, dependent: :destroy
 
     validates :title, presence: true, length: { minimum: 5 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200416174800) do
+ActiveRecord::Schema.define(version: 20200428191546) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,18 @@ ActiveRecord::Schema.define(version: 20200416174800) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.bigint   "image_file_size"
+    t.datetime "image_updated_at"
+    t.integer  "article_id"
+    t.index ["article_id"], name: "index_photos_on_article_id"
   end
 
   create_table "users", force: :cascade do |t|
